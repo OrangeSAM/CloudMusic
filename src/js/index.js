4a1442b31d15764fb439ddbb5982ad2339c8c59e
sam <amazing_sam@outlook.com>
2019-02-16 23:41:48 +0800

    almost finished this project

--- src/js/index.js
+++ src/js/index.js
@@ -1,59 +1,70 @@
-var APP_ID = '8FXe5ffF8dBGfBzQrJDXJPNB-gzGzoHsz';
-var APP_KEY = 'fvva0OpwSWKOUx1dpYttFSpb';
+var APP_ID = "8FXe5ffF8dBGfBzQrJDXJPNB-gzGzoHsz";
+var APP_KEY = "fvva0OpwSWKOUx1dpYttFSpb";
 
 AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
+  appId: APP_ID,
+  appKey: APP_KEY
 });
-var query = new AV.Query('song');
-let songlist = document.querySelector('.songList');
-query.find().then(function (song) {
+var query = new AV.Query("song");
+let songlist = document.querySelector(".songList");
+query
+  .find()
+  .then(function (song) {
     song.forEach(function (object) {
-        let singlesong = document.createElement('div');
-        singlesong.innerHTML = `
-        <div class="singleSong">    
+      let singlesong = document.createElement("div");
+      singlesong.classList.add("singleSong");
+      singlesong.classList.add('hereforclick');
+      singlesong.setAttribute('data-songId', object.id)
+      singlesong.innerHTML = `
             <div>
                 <div class="songName">${object.attributes.songName}</div>
                 <div class="songDetail">
                     <svg class="icon" aria-hidden="true">
                         <use xlink:href="#icon-wusunyinzhi"></use>
                     </svg>
-                    <span class="singer">${object.attributes.singer}</span> -<span class="album">飞行器的执行周期</span>
+                    <span class="singer">${
+                      object.attributes.singer
+                    }</span> -<span class="album">飞行器的执行周期</span>
                 </div>
             </div>
-            <div class="hereforclick" data-url="${object.attributes.externalUrl}">
+            <div>
                 <svg class="icon" aria-hidden="true">
                     <use xlink:href="#icon-bofang"></use>
                 </svg>
-            </div>
-        </div>`;
-        songlist.appendChild(singlesong);
-        //但是这样就多了一个div元素，之前是直接append反字符串内容 所以没用 ，解决方法只有jquery吗?
-        //待做  页面间的数据传递
+            </div>`;
+      songlist.appendChild(singlesong);
+      //但是这样就多了一个div元素，之前是直接append反字符串内容 所以没用 ，解决方法只有jquery吗?
+      //已解决 view里少个div不就行了
+      //待做  页面间的数据传递
     });
-}).then(function () {
-    let clickEvent = document.querySelectorAll('.hereforclick');
-    // clickEvent.forEach((curv) => {
-    //     console.log(curv);
-    //     addEventListener('click', function (e) {
-    //         e.preventDefault();
-    //         console.log(e);
-    //         console.log(e.target.dataset.url);
-    //         let a = e.target.dataset.url;
-    //         sessionStorage.setItem('externalUrl', a);
-    //         // window.location.href = 'http://127.0.0.1:8080/src/song.html';
-    //     })
-    // });
-    for (let i = 0; i < clickEvent.length; i++) {
-        clickEvent[i].addEventListener('click', function (e) {
+  })
+  .then(
+    function () {
+      let clickEvent = document.querySelectorAll(".hereforclick");
+      // clickEvent.forEach((curv) => {
+      //     console.log(curv);
+      //     addEventListener('click', function (e) {
+      //         e.preventDefault();
+      //         console.log(e);
+      //         console.log(e.target.dataset.url);
+      //         let a = e.target.dataset.url;
+      //         sessionStorage.setItem('externalUrl', a);
+      //         // window.location.href = 'http://127.0.0.1:8080/src/song.html';
+      //     })
+      // });
+      for (let i = 0; i < clickEvent.length; i++) {
+        clickEvent[i].addEventListener(
+          "click",
+          function (e) {
             e.preventDefault();
-            console.log(e);
-            console.log(e.path[2]);
-            let a = e.target.dataset.url;
-            sessionStorage.setItem('externalUrl', a);
-            // window.location.href = 'http://127.0.0.1:8080/src/song.html';
-        }, true)
+            let songId = e.target.dataset.songid;
+            sessionStorage.setItem("songId", songId);
+            window.location.href = 'http://127.0.0.1:8080/src/song.html';
+          }, true
+        );
+      }
+    },
+    function (error) {
+      console.log(error); // 异常处理
     }
-}, function (error) {
-    console.log(error); // 异常处理
-});
+  );
--- src/js/songplay.js
+++ src/js/songplay.js
@@ -1,32 +1,85 @@
-function getlyric() {
-    $.get("./js/lyric.json").then(
-        lyric => {
-            let array = lyric.lyric.split("\n");
-            let regex = /^\[(.+)\](.*)/;
-            array = array.map(function (string) {
-                let matches = string.match(regex);
-                if (matches) {
-                    let para = document.createElement("p");
-                    let lyric = $(".lyric-detail");
-                    para.innerText = matches[2];
-                    para.setAttribute("data-time", matches[1]);
-                    lyric.append(para);
-                    return {
-                        time: matches[1],
-                        words: matches[2]
-                    };
-                }
-            });
-        },
-        err => {
-            console.log(err);
-        }
-    );
-}
-getlyric();
+var APP_ID = "8FXe5ffF8dBGfBzQrJDXJPNB-gzGzoHsz";
+var APP_KEY = "fvva0OpwSWKOUx1dpYttFSpb";
 
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+let songId = sessionStorage.getItem("songId");
+
+var querySong = new AV.Query("song");
 let audioEle = document.createElement("audio");
-audioEle.src = sessionStorage.getItem('externalUrl') || 'http://pla636c1g.bkt.clouddn.com/%E4%BE%A3%E8%A1%8C%E8%83%8C%E6%99%AF%E9%9F%B3%E4%B9%90.mp3';
+
+let lyric = document.querySelector('.lyric');
+let lyricCont = document.querySelector(".lyric-detail");
+let lyricWrap = document.querySelector(".lyric-wrap");
+
+querySong.get(songId).then((response) => {
+    console.log(response);
+    audioEle.src = response.attributes.externalUrl || "http://sam.cloudmusic.yuanq.club/%E5%BC%A0%E5%9B%BD%E8%8D%A3%20-%20%E4%BA%BA%E7%94%9F%E8%B7%AF.mp3";
+    let songTitle = document.createElement("h3");
+    let songTitleCont = response.attributes.songName + ' - ' + response.attributes.singer;
+    songTitle.innerText = songTitleCont;
+    document.getElementById("sitetitle").innerText = songTitleCont;
+    lyric.insertBefore(songTitle, lyricCont);
+    formatLyric(response.attributes.lyric.lyric);
+    //获取歌词这里踩了大坑  应该用对象格式存
+})
+
+function formatLyric(lyric) {
+    let array = lyric.split("\n");
+    let regex = /^\[(.+)\](.*)/;
+    array = array.map(function (string) {
+        let matches = string.match(regex);
+        if (matches) {
+            let para = document.createElement("p");
+            para.innerText = matches[2];
+            para.setAttribute("data-time", matches[1]);
+            lyricWrap.append(para);
+            return {
+                time: matches[1],
+                words: matches[2]
+            };
+        }
+    });
+}
+
+// function getlyric() {
+//     $.get("./js/lyric11.json").then(
+//         lyric => {
+//             let array = lyric.lyric.split("\n");
+//             console.log(array);
+//             let regex = /^\[(.+)\](.*)/;
+//             array = array.map(function (string) {
+//                 let matches = string.match(regex);
+//                 if (matches) {
+//                     let para = document.createElement("p");
+//                     let lyric = $(".lyric-detail");
+//                     para.innerText = matches[2];
+//                     para.setAttribute("data-time", matches[1]);
+//                     lyric.append(para);
+//                     return {
+//                         time: matches[1],
+//                         words: matches[2]
+//                     };
+//                 }
+//             });
+//         },
+//         err => {
+//             console.log(err);
+//         }
+//     );
+// }
+// getlyric();
+
+
+audioEle.addEventListener('ended', function () {
+    $(".disc-ring").addClass("playing");
+    $(".disc-default").addClass("playing");
+    $(".cover").addClass("playing");
+})
+
+
 let $playbtn = $(".disc");
 let $playicon = $(".playbtn");
 
