779605bf97fd3df269ede385badf14f469378ea8
sam <amazing_sam@outlook.com>
2019-02-19 00:47:18 +0800

    finished  the  hot music and search section

--- src/js/songplay.js
+++ src/js/songplay.js
@@ -10,26 +10,52 @@ let songId = sessionStorage.getItem("songId");
 var querySong = new AV.Query("song");
 let audioEle = document.createElement("audio");
 
-let lyric = document.querySelector('.lyric');
+let lyric = document.querySelector(".lyric");
 let lyricCont = document.querySelector(".lyric-detail");
 let lyricWrap = document.querySelector(".lyric-wrap");
 
-querySong.get(songId).then((response) => {
-    console.log(response);
-    audioEle.src = response.attributes.externalUrl || "http://sam.cloudmusic.yuanq.club/%E5%BC%A0%E5%9B%BD%E8%8D%A3%20-%20%E4%BA%BA%E7%94%9F%E8%B7%AF.mp3";
+querySong.get(songId).then(response => {
+    audioEle.src =
+        response.attributes.externalUrl ||
+        "http://sam.cloudmusic.yuanq.club/%E5%BC%A0%E5%9B%BD%E8%8D%A3%20-%20%E4%BA%BA%E7%94%9F%E8%B7%AF.mp3";
     let songTitle = document.createElement("h3");
-    let songTitleCont = response.attributes.songName + ' - ' + response.attributes.singer;
+    let songTitleCont =
+        response.attributes.songName + " - " + response.attributes.singer;
     songTitle.innerText = songTitleCont;
     document.getElementById("sitetitle").innerText = songTitleCont;
     lyric.insertBefore(songTitle, lyricCont);
     formatLyric(response.attributes.lyric.lyric);
     //获取歌词这里踩了大坑  应该用对象格式存
-})
+});
+
+audioEle.ontimeupdate = function (e) {
+    let time = e.path[0].currentTime;
+    let curTime =
+        "0" +
+        (time / 6000).toFixed() +
+        ":" +
+        (time % 1000 > 10 ? "" : "0") +
+        (time % 1000).toFixed(3);
+    for (let i = 0; i < lyricArray.length; i++) {
+        if (curTime > lyricArray[i]["time"]) {
+            let para1 = document.createElement("p");
+            para1.innerText = lyricArray[i]["words"];
+            console.log(para1);
+            let para2 = document.createElement("p");
+            para2.innerText = lyricArray[i + 1]["words"];
+            lyricWrap.append = para1;
+            // let count = 0;
+            // count += 1
+            // lyricCont.setAttribute('style', "transform:translateY(" + (-2) * count + "em" + ")")
+        }
+    }
+};
+var lyricArray;
 
 function formatLyric(lyric) {
-    let array = lyric.split("\n");
+    lyricArray = lyric.split("\n");
     let regex = /^\[(.+)\](.*)/;
-    array = array.map(function (string) {
+    lyricArray = lyricArray.map(function (string) {
         let matches = string.match(regex);
         if (matches) {
             let para = document.createElement("p");
@@ -44,41 +70,12 @@ function formatLyric(lyric) {
     });
 }
 
-// function getlyric() {
-//     $.get("./js/lyric11.json").then(
-//         lyric => {
-//             let array = lyric.lyric.split("\n");
-//             console.log(array);
-//             let regex = /^\[(.+)\](.*)/;
-//             array = array.map(function (string) {
-//                 let matches = string.match(regex);
-//                 if (matches) {
-//                     let para = document.createElement("p");
-//                     let lyric = $(".lyric-detail");
-//                     para.innerText = matches[2];
-//                     para.setAttribute("data-time", matches[1]);
-//                     lyric.append(para);
-//                     return {
-//                         time: matches[1],
-//                         words: matches[2]
-//                     };
-//                 }
-//             });
-//         },
-//         err => {
-//             console.log(err);
-//         }
-//     );
-// }
-// getlyric();
-
-
-audioEle.addEventListener('ended', function () {
+//控制歌听后封面停转
+audioEle.addEventListener("ended", function () {
     $(".disc-ring").addClass("playing");
     $(".disc-default").addClass("playing");
     $(".cover").addClass("playing");
-})
-
+});
 
 let $playbtn = $(".disc");
 let $playicon = $(".playbtn");
